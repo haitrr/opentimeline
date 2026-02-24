13108653a65487ceeb8898340d22d41a27bbd641
haitrr <h@hai.fyi>
2026-02-24 11:53:04 +0700

    fit on load

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 import { format } from "date-fns";
 
 export default function RootPage() {
-  redirect(`/timeline/${format(new Date(), "yyyy-MM-dd")}`);
+  redirect(`/timeline/${format(new Date(), "yyyy-MM-dd")}?fit=1`);
 }
