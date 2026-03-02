6f9c4ab009a190b5a4e85ff0ade25a15d56db977
haitrr <h@hai.fyi>
2026-03-02 10:16:18 +0700

    fix date

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { format } from "date-fns";
 
 export default function RootPage() {
-  redirect(`/timeline/${format(new Date(), "yyyy-MM-dd")}?fit=1`);
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(`/timeline/${format(new Date(), "yyyy-MM-dd")}?fit=1`);
+  }, [router]);
+
+  return null;
 }
